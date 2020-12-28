c5e46df7fe1720b319f1f177bc13dfc240c8a470
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-28 14:53:56 +0200

    refract controllers

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const Blog = require('./models/blog');
-
+const blogsRouter = require('./controllers/blogs');
 const app = express();
 
 // const mongoUrl = 'mongodb://localhost/bloglist'
@@ -18,20 +17,7 @@ mongoose.connect(mongoUrl, {
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api', blogsRouter);
 app.use(logger.reqDetail);
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 module.exports = app;
