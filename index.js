4a9d2ddac8b59c0a52067f42517f27b30974a26b
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-28 13:59:23 +0200

    refract utils

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 require('dotenv').config();
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
+
+const app = express();
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
 // const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl=process.env.MONGODB_URL
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const mongoUrl = process.env.MONGODB_URL;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const morgan = require('morgan');
 morgan.token('req-body', (req) => JSON.stringify(req.body));
 app.use(
   morgan(
@@ -30,24 +37,20 @@ app.use(
 );
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = process.env.PORT
+const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
