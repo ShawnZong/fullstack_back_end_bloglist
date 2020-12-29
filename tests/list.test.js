13b6bf07b13a117bf8bf44bed1daaff1ca98d0e1
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2020-12-29 15:02:44 +0200

    refract to async/await

--- tests/list.test.js
+++ tests/list.test.js
@@ -1,32 +1,4 @@
-const mongoose = require('mongoose');
-const supertest = require('supertest');
 const listHelper = require('../utils/list_helper');
-const app = require('../app');
-const Blog = require('../models/blog');
-
-const api = supertest(app);
-
-describe('api test when feed in some initial blogs', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
-    // eslint-disable-next-line no-restricted-syntax
-    for (const blog of listHelper.initialBlogs) {
-      const blogObject = new Blog(blog);
-      // eslint-disable-next-line no-await-in-loop
-      await blogObject.save();
-    }
-  });
-  test.only('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-  });
-  test.only('the right amount of blogs are returned', async () => {
-    const response = await api.get('/api/blogs');
-    expect(response.body.length).toBe(listHelper.initialBlogs.length);
-  });
-});
 
 const blogs = [
   {
@@ -138,8 +110,4 @@ describe('total likes', () => {
       likes: 17,
     });
   });
-});
-
-afterAll(() => {
-  mongoose.connection.close();
 });
